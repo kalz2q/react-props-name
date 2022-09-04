663c658943f363b8efdb8f7b1b06f61ede8f9c9d
kalz2q <katsu95i@gmail.com>
2022-09-04 09:59:51 +0900

    map

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,23 @@ const Welcome = (props: WelcomeProps) => {
   return <h1>Hello, {props.name}</h1>;
 };
 
+const welcomes = () => {
+  return (
+    <>
+      <li>this</li>
+      <li>this</li>
+      <li>this</li>
+    </>
+  );
+};
+
 function App() {
   return (
     <div className="App">
       <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Ted" />
+      <ol>{welcomes()}</ol>
     </div>
   );
 }
